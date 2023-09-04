8e3878fb9cf5dd70ab5fe1b33e548ddca00a952f
FernandoFS-Dev <fernandosantossander@gmail.com>
2023-09-04 17:35:00 -0300

    second commit

---
+++ src/CriacaoTarefas/ListaTarefa.java
@@ -0,0 +1,51 @@
+package CriacaoTarefas;
+
+import java.util.ArrayList;
+import java.util.List;
+public class ListaTarefa {
+    //atributo
+    private List<Tarefa> tarefaList;
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefaParaRemover = new ArrayList<>();
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefaParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefaParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println("O número total de elementos na lista é " + listaTarefa.obterNumeroTotalTarefas());
+        listaTarefa.obterDescricoesTarefas();
+
+        listaTarefa.adicionarTarefa("Tarefa1");
+        listaTarefa.adicionarTarefa("Tarefa1");
+        listaTarefa.adicionarTarefa("Tarefa2");
+        System.out.println("O número total de elementos na lista é " + listaTarefa.obterNumeroTotalTarefas());
+        listaTarefa.obterDescricoesTarefas();
+
+        listaTarefa.removerTarefa("Tarefa1");
+        System.out.println("O número total de elementos na lista é " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+
+    }
+}
---
+++ src/CriacaoTarefas/Tarefa.java
@@ -0,0 +1,20 @@
+package CriacaoTarefas;
+
+public class Tarefa {
+    //atributo
+
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+    public String toString() {
+        return descricao;
+    }
+}
